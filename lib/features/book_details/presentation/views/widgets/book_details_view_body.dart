2019d66e0b1273a46731ddd2d37e7a7e5f1455d0
kareemkamal11 <sefi31009853@f-eng.tanta.edu.eg>
2024-07-08 16:47:10 +0300

    Refactor book details view and body widgets

--- lib/features/book_details/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/book_details/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
@@ -12,12 +14,24 @@ class BookDetailsViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const CustomDetailsAppBar(),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * .17),
             child: const CustomBookImage(),
-          )
+          ),
+          const SizedBox(height: 32),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textLarge,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+          ),
+          const SizedBox(height: 13),
+          const Text('Rudyard Kipling', style: Styles.textSmall),
+          const SizedBox(height: 30),
+          const BookRating()
         ],
       ),
     );
--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -9,6 +9,7 @@ class BookRating extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      
       children: [
         const Icon(
           FontAwesomeIcons.solidStar,
